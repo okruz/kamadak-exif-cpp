369b4951fd9ec20000f649d6fd71d004aa705c91
okruz <okruz@github.com>
2024-05-12 16:36:11 +0200

    Added memory management.

--- src/interface.rs
+++ src/interface.rs
@@ -6,8 +6,35 @@
  */
 
 use exif::{Exif, In, Reader, Tag};
+use std::ffi::{c_char, CString};
 use std::os::raw::c_void;
 
+/// Internal memory management struct. Couples the lifetimes of the returned CStrings to that of the parsed Exif data.
+pub(crate) struct ExifScope {
+    /// The parsed exif data from the kadamak crate
+    pub(crate) exif: Exif,
+    /// The CStrings returned as "*const char" over the ffi interface. Once this struct is dropped,
+    /// the CStrings will be dropped as well.
+    cstrings: Vec<CString>,
+}
+
+impl ExifScope {
+    /// Converts a &str to a CString and adds it to the ExifScope, such that it will be dropped, 
+    /// once the ExifScope is dropped. Returns the contents of the CString as a null-terminated
+    /// "*const char"-pointer.
+    pub(crate) fn add_string(&mut self, data: &str) -> Result<*const c_char, ErrorCodes> {
+        self.cstrings
+            .push(CString::new(data).map_err(|_| ErrorCodes::UnknownError)?);
+        Ok(self
+            .cstrings
+            .last()
+            .ok_or(ErrorCodes::UnknownError)?
+            .as_bytes_with_nul()
+            .as_ptr() as *const c_char)
+    }
+}
+
+/// The opaque struct returned over the ffi interface. The C/C++ side shall not be concerned with the internal representation anyway.
 #[repr(C)]
 pub struct ExifData {
     val: *mut c_void,
@@ -20,40 +47,62 @@ impl ExifData {
         }
     }
 
+    // Checks whether self holds any data.
     pub(crate) fn is_null(&self) -> bool {
         self.val.is_null()
     }
 
     pub(crate) fn from_exif(exif: Exif) -> Self {
-        // Move exif onto the heap and make the box give up ownership.
-        let exif_ptr: *mut Exif = Box::into_raw(Box::new(exif));
+        // Move exif onto the heap and make the box give up ownership to extend the lifetime until drop_explicitly() is called.
+        let exif_scope_ptr: *mut ExifScope = Box::into_raw(Box::new(ExifScope {
+            exif: exif,
+            cstrings: vec![],
+        }));
         Self {
-            val: exif_ptr as *mut c_void,
+            val: exif_scope_ptr as *mut c_void,
         }
     }
 
-    pub(crate) fn to_exif(self) -> Result<&'static Exif, ErrorCodes> {
+    /// Casts the val pointer to a shared reference to an ExifScope.
+    pub(crate) fn to_exif_scope(self) -> Result<&'static ExifScope, ErrorCodes> {
         if self.is_null() {
             return Err(ErrorCodes::Nullptr);
         }
 
-        Ok(unsafe { &*(self.val as *mut Exif) })
+        // Is safe as long as there is no concurrent use of a mutable reference to the ExifScope,
+        // self was created via from_exif and has not been dropped explicitly.
+        // It is upon the user of the ffi interface to make sure these assumptions hold true.
+        Ok(unsafe { &*(self.val as *const ExifScope) })
     }
 
+    /// Casts the val pointer to a mutable reference to an ExifScope.
+    pub(crate) fn to_exif_scope_mut(self) -> Result<&'static mut ExifScope, ErrorCodes> {
+        if self.is_null() {
+            return Err(ErrorCodes::Nullptr);
+        }
+
+        // Is safe as long as self is not used concurrently, it was created via from_exif and has not been dropped explicitly.
+        // It is upon the user of the ffi interface to make sure these assumptions hold true.
+        Ok(unsafe { &mut *(self.val as *mut ExifScope) })
+    }
+
+    /// Drops and deallocates the data pointed to to by self.val.
     pub(crate) fn drop_explicitly(self) -> ErrorCodes {
         if self.is_null() {
             return ErrorCodes::Nullptr;
         }
 
-        // Once the box goes out of scope, the Exif struct on the heap will be dropped and deallocated as well.
-        let _box_to_drop = unsafe { Box::from_raw(self.val as *mut Exif) };
+        // Once the box goes out of scope, the ExifScope struct on the heap will be dropped and deallocated as well.
+        let _box_to_drop = unsafe { Box::from_raw(self.val as *mut ExifScope) };
         ErrorCodes::Ok
     }
 }
 
 #[repr(C)]
 pub struct ExifParseResult {
+    /// Opaque pointer to the parsed data to be returned via the ffi interface.
     pub(crate) data: ExifData,
+    /// Indicates whether the parsing was successful.
     pub(crate) error_code: ErrorCodes,
 }
 
@@ -78,4 +127,5 @@ pub enum ErrorCodes {
     Ok,
     Nullptr,
     ParseError,
+    UnknownError,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@
 mod interface;
 use crate::interface::*;
 use exif::{Exif, In, Reader, Tag};
+use std::ffi::c_char;
 use std::io::Cursor;
 
 /// Loads the exif data from encoded image.
@@ -39,6 +40,7 @@ pub extern "C" fn EXIF_load(data: *const u8, length: usize) -> ExifParseResult {
 /// Frees the allocated memory.
 ///
 /// Frees all data allocated during the extraction of the EXIF information.
+/// If the returned ErrorCodes is not ErrorCodes::Ok, the deallocation could not be performed.
 #[no_mangle]
 pub extern "C" fn EXIF_free(data: ExifData) -> ErrorCodes {
     data.drop_explicitly()
@@ -47,13 +49,29 @@ pub extern "C" fn EXIF_free(data: ExifData) -> ErrorCodes {
 /// Returns the little endian attribute from the EXIF header.
 ///
 /// Indicates whether or not the data in the EXIF fields are encoded in little endian or big endian
-/// byte order.
+/// byte order. If the returned ErrorCodes is not ErrorCodes::Ok, the value could not be extracted.
 #[no_mangle]
 pub extern "C" fn EXIF_is_little_endian(data: ExifData, little_endian: &mut bool) -> ErrorCodes {
-    let exif = match data.to_exif() {
+    let exif_scope = match data.to_exif_scope() {
         Err(error_code) => return error_code,
         Ok(val) => val,
     };
-    *little_endian = exif.little_endian();
+    *little_endian = exif_scope.exif.little_endian();
+    ErrorCodes::Ok
+}
+
+#[no_mangle]
+pub extern "C" fn EXIF_get_some_string(data: ExifData, string: *mut *const c_char) -> ErrorCodes {
+    let exif_scope = match data.to_exif_scope() {
+        Err(error_code) => return error_code,
+        Ok(val) => val,
+    };
+
+    unsafe {
+        *string = match exif_scope.add_string("Hello World") {
+            Err(error_code) => return error_code,
+            Ok(val) => val as *const c_char,
+        };
+    }
     ErrorCodes::Ok
 }
