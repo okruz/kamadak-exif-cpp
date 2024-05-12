adef868357ceb92b0bdd7cf78c333cc865d56d4b
okruz <okruz@github.com>
2024-05-12 15:02:03 +0200

    Added comments

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,14 @@ use crate::interface::*;
 use exif::{Exif, In, Reader, Tag};
 use std::io::Cursor;
 
+/// Loads the exif data from encoded image.
+///
+/// Attempts to parse the EXIF data from the provided data. If the parsing was unsuccessful,
+/// the ExifParseResult::error_code will indicate what went wrong and the ExifParseResult::data::val
+/// pointer will be a nullptr. Else, the the ExifParseResult::error_code will ErrorCodes::Ok and the
+/// ExifParseResult::data::val will point to the parsed data.
 #[no_mangle]
-pub extern "C" fn EXIF_load_exif(data: *const u8, length: usize) -> ExifParseResult {
+pub extern "C" fn EXIF_load(data: *const u8, length: usize) -> ExifParseResult {
     if data.is_null() {
         return ExifParseResult::make_null();
     }
@@ -30,11 +36,18 @@ pub extern "C" fn EXIF_load_exif(data: *const u8, length: usize) -> ExifParseRes
     }
 }
 
+/// Frees the allocated memory.
+///
+/// Frees all data allocated during the extraction of the EXIF information.
 #[no_mangle]
-pub extern "C" fn EXIF_free_exif(data: ExifData) -> ErrorCodes {
+pub extern "C" fn EXIF_free(data: ExifData) -> ErrorCodes {
     data.drop_explicitly()
 }
 
+/// Returns the little endian attribute from the EXIF header.
+///
+/// Indicates whether or not the data in the EXIF fields are encoded in little endian or big endian
+/// byte order.
 #[no_mangle]
 pub extern "C" fn EXIF_is_little_endian(data: ExifData, little_endian: &mut bool) -> ErrorCodes {
     let exif = match data.to_exif() {
